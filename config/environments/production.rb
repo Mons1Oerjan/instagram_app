1a786b7e1886a90904e3dc589a3dc06903858f5e
Orjan Monsen <mons@dal.ca>
2017-01-12 18:49:05 -0400

    added image upload functionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,10 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # Paperclip config:
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
